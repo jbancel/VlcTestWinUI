21086b0498c394c44eba89db280e82fd71b09825
Jean-Baptiste <courgette.filante@gmail.com>
2025-11-13 15:47:57 +0100

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,27 @@
+using System;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
+using LibVLCSharp.Shared;
 using LibVLCSharp.Platforms.Windows;
 
 namespace VlcTest
 {
     public sealed partial class MainWindow : Window
     {
+        LibVLC libvlc;
+        MediaPlayer mp;
+
         public MainWindow()
         {
             InitializeComponent();
-            Ui_MainGrid.Loaded += (s, e) =>
-            {
-                VideoView videoView = new();
-                ((Grid)s).Children.Add(videoView);
-            };
+            VideoView.Initialized += VideoView_Initialized;
+        }
+
+        private void VideoView_Initialized(object sender, InitializedEventArgs e)
+        {
+            libvlc = new LibVLC(enableDebugLogs: true, e.SwapChainOptions);
+            mp = new MediaPlayer(libvlc);
+            using var media = new Media(libvlc, new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"));
+            mp.Play(media);
         }
     }
 }
